e3fa6e21bfabcc8f050e90003b0a29df1da45cdf
shellieD <shellie.downie13@gmail.com>
2022-10-27 23:51:15 +0000

    Added stripe elements.:

--- get_ducked/checkout/views.py
+++ get_ducked/checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LKH8SAWtvFHMlJiXxWx0rw1PA5yXUBFguXq5Ch0Vcuj9MJCd5XrjP9C1VHsAVOyBqyP8v6uQWUr2oz9rEcr3lWk00K6aDxtQm',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
