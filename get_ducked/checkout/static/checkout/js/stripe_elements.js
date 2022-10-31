821a08769a6f1983f749c38dd78961209c4d746e
shellieD <shellie.downie13@gmail.com>
2022-10-31 02:11:12 +0000

    FEATURE: Added loading overlay.

--- get_ducked/checkout/static/checkout/js/stripe_elements.js
+++ get_ducked/checkout/static/checkout/js/stripe_elements.js
@@ -55,6 +55,8 @@ form.addEventListener('submit', function (event) {
         'disabled': true
     });
     $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -68,10 +70,12 @@ form.addEventListener('submit', function (event) {
             </span>
             <span>${result.error.message}</span> `;
             $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
             card.update({
                 'disabled': false
             });
-            $('${#submit-button').attr('disabled', false);
+            $('#submit-button').attr('disabled', false);
         } else {
             if (result.paymentIntent.status === 'succeeded') {
                 form.submit();
