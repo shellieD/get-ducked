d4e1ed788627cf457d65806614d4e05f37715768
shellieD <shellie.downie13@gmail.com>
2022-10-31 01:43:52 +0000

    FEATURE: Added order summary to checkout page.

--- get_ducked/products/models.py
+++ get_ducked/products/models.py
@@ -14,8 +14,8 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-        def get_friendly_name(self):
-            return self.friendly_name
+    def get_friendly_name(self):
+        return self.friendly_name
 
 
 class Product(models.Model):
