c153b029883281f1ee615261e4b5f27ca6420b39
shellieD <shellie.downie13@gmail.com>
2022-11-07 18:30:06 +0000

    Removed secret key and set debug.

--- get_ducked/settings.py
+++ get_ducked/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ecu1orb$n%7fbunkdt+l=rxjx-j6#$)92vthd$(w4bth!@bf$g'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['get-ducked.herokuapp.com', 'localhost']
 
