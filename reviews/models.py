8283f96de39c7dcedb6cecff53d7252a2abd891d
shellieD <shellie.downie13@gmail.com>
2022-11-16 22:42:37 +0000

    Created Reviews Model.

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from products.models import Product
+from profiles.models import UserProfile
 
-# Create your models here.
+
+class Reviews(models.Model):
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    subject = models.Charfield(max_length=100, blank=True)
+    review = models.TextField(max_length=500, blank=True)
+    rating = models.IntegerField()
+    status = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.subject
