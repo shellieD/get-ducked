0c67cf7b70b4c1a6cd1a1593d9479ff830cd546b
shellieD <shellie.downie13@gmail.com>
2022-11-27 16:10:03 +0000

    Remove send_email functionality from checkout views.py.

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -44,7 +44,6 @@ class StripeWH_Handler:
         pid = intent.id
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
-        print(save_info)
 
         billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
